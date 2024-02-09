149bcd1f2727f02b2fe490358dc05add617f9f52
Computer Crunch <100100087+computercrunchnz@users.noreply.github.com>
2024-02-10 11:10:31 +1300

    Update crunchbench.sh

--- crunchbench.sh
+++ crunchbench.sh
@@ -1,55 +1,41 @@
 echo "Starting CrunchBench..."
 echo "Running PyBench..."
-python3 pybench.py
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp
-python3 pybench.py
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp
-python3 pybench.py
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp
+cat /sys/class/thermal/thermal_zone*/temp > tempbeforepybench.txt
+python3 pybench.py > pybenchrun1.txt
+cat /sys/class/thermal/thermal_zone*/temp > temppybenchrun1.txt
+python3 pybench.py > pybenchrun2.txt
+cat /sys/class/thermal/thermal_zone*/temp > temppybenchrun2.txt
+python3 pybench.py > pybenchrun3.txt
+cat /sys/class/thermal/thermal_zone*/temp > temppybenchrun3.txt
 echo "PyBench Finished. Cooling for 60 seconds."
 sleep 60
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp
+cat /sys/class/thermal/thermal_zone*/temp > tempbeforeglmark2.txt
 echo "Running glmark2..."
 glmark2
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp
+cat /sys/class/thermal/thermal_zone*/temp > tempglmark2run1.txt
 glmark2
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp
+cat /sys/class/thermal/thermal_zone*/temp > tempglmark2run2.txt
 glmark2
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp
+cat /sys/class/thermal/thermal_zone*/temp > tempglmark2run3.txt
 echo "glmark2 finished. Cooling for 60 seconds."
 sleep 60
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp
+cat /sys/class/thermal/thermal_zone*/temp > tempsysbenchsinglebefore.txt
 echo "Running sysbench single thread..."
 sysbench cpu --threads=1 --time=60 run
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp
+cat /sys/class/thermal/thermal_zone*/temp > tempsysbenchsinglerun1.txt
 sysbench cpu --threads=1 --time=60 run
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp
+cat /sys/class/thermal/thermal_zone*/temp > tempsysbenchsinglerun2.txt
 sysbench cpu --threads=1 --time=60 run
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp
+cat /sys/class/thermal/thermal_zone*/temp > tempsysbenchsinglerun3.txt
 echo "sysbench single thread finished. Cooling for 60 seconds."
 sleep 60
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp'
+cat /sys/class/thermal/thermal_zone*/temp > tempsysbenchmultibefore.txt
 echo "Running sysbench multi thread..."
 sysbench cpu --threads=`nproc --all` --time=60 run
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp
+cat /sys/class/thermal/thermal_zone*/temp > tempsysbenchmultirun1.txt
 sysbench cpu --threads=`nproc --all` --time=60 run
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp
+cat /sys/class/thermal/thermal_zone*/temp > tempsysbenchmultirun2.txt
 sysbench cpu --threads=`nproc --all` --time=60 run
-echo "CPU Temp:" 
-cat /sys/class/thermal/thermal_zone*/temp
+cat /sys/class/thermal/thermal_zone*/temp > tempsysbenchmultirun3.txt
 echo "sysbench multi thread finished."
 echo "CrunchBench Finished!"
